41d163399c3ab68bb0eb6422b8ae07d4a74ccd56
Vitor Oliveira <vitoro.fabiano@gmail.com>
2024-10-13 23:30:23 -0300

    feat: create project,  config font model and create a Header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,28 @@
+// components/Header.tsx
+import Link from "next/link";
+
+export default function Header() {
+  return (
+    <header className="  p-4">
+      <nav className="container mx-auto flex justify-between items-center">
+        <Link href="/" className="text-3xl font-bold">
+          moviedex
+        </Link>
+        <ul className="flex space-x-4">
+          <li>
+            <Link href="/filmes" className="hover:underline">
+              Filmes
+            </Link>
+          </li>
+          <li>
+            <Link href="/avaliacoes" className="hover:underline">
+              Avaliações
+            </Link>
+          </li>
+        </ul>
+        <div></div>
+        <div className="flex">Entre ou Registre-se</div>
+      </nav>
+    </header>
+  );
+}
